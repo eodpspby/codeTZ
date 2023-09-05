4c5d4e50325399948751357a6afdfd7b920414db
Egor Danilkovich <eodpsp@yandex.ru>
2023-09-05 18:10:22 +0300

    First commit

---
+++ storage/cache/b7/b7fd9280e02644aec4a139e03660e952f0296aa21980549a9a3cb5449639ce9d.php
@@ -0,0 +1,50 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_54e02b09118581fb4e3d92236152e9dec521d0e08970e57d587a00cfeccc8730 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\">
+    <a href=\"http://opencart.ru\" target=\"_blank\">";
+        // line 3
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>
+  </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 3,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container">*/
+/*     <a href="http://opencart.ru" target="_blank">{{ text_project }}</a>*/
+/*   </div>*/
+/* </footer>*/
+/* </body></html>*/
